0acaaffd53f07ce913c873dc1abaeb5aa7e8bf79
Maria Paz Rojas <mrojas@rayensalud.com>
2018-06-25 12:29:46 -0400

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  API_ENDPOINT: 'http://172.16.3.91/RayenSalud.WebLun.Api/api/',
+  DIRECCION: 'Merced 480 Piso 2, Santiago - Chile',
+  TELEFONO:'(+56 2) 2588 8800',
+  CONTACTO:'contacto@rayensalud.com',
+  WEB:'https://www.rayensalud.com/',
+  FACEBOOK:'https://www.facebook.com/Rayen-848452161931776',
+  TWITTER:'https://twitter.com/Rayen_Salud',
 };
